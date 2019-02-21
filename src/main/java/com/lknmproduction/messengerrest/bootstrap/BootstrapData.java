bf453169943414a63d688f91791f1254c861a1f3
Kirill Kundik <goliathusua@gmail.com>
2019-02-21 19:06:15 +0200

    commented rows for bootstrapping data

--- src/main/java/com/lknmproduction/messengerrest/bootstrap/BootstrapData.java
+++ src/main/java/com/lknmproduction/messengerrest/bootstrap/BootstrapData.java
@@ -17,24 +17,24 @@ public class BootstrapData implements CommandLineRunner {
     @Override
     public void run(String... args) throws Exception {
 
-        System.out.println("Loading Customer Data");
-
-        Customer c1 = new Customer();
-        c1.setFirstname("Michale");
-        c1.setLastname("Weston");
-        customerRepository.save(c1);
-
-        Customer c2 = new Customer();
-        c2.setFirstname("John");
-        c2.setLastname("Wick");
-        customerRepository.save(c2);
-
-        Customer c3 = new Customer();
-        c3.setFirstname("James");
-        c3.setLastname("Born");
-        customerRepository.save(c3);
-
-        System.out.println("Customers Saved: " + customerRepository.count());
+//        System.out.println("Loading Customer Data");
+//
+//        Customer c1 = new Customer();
+//        c1.setFirstname("Michale");
+//        c1.setLastname("Weston");
+//        customerRepository.save(c1);
+//
+//        Customer c2 = new Customer();
+//        c2.setFirstname("John");
+//        c2.setLastname("Wick");
+//        customerRepository.save(c2);
+//
+//        Customer c3 = new Customer();
+//        c3.setFirstname("James");
+//        c3.setLastname("Born");
+//        customerRepository.save(c3);
+//
+//        System.out.println("Customers Saved: " + customerRepository.count());
 
     }
 }
