d405a41cea39c35969d9988e9295da4f4f3c5667
kyrylo_kundik <goliathusua@gmail.com>
2019-03-09 20:33:50 +0200

    added processing variables

--- src/main/java/com/lknmproduction/messengerrest/controllers/UserController.java
+++ src/main/java/com/lknmproduction/messengerrest/controllers/UserController.java
@@ -2,17 +2,30 @@ package com.lknmproduction.messengerrest.controllers;
 
 import com.lknmproduction.messengerrest.domain.User;
 import com.lknmproduction.messengerrest.service.UserService;
+import com.twilio.jwt.accesstoken.AccessToken;
+import com.twilio.jwt.accesstoken.ChatGrant;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@PropertySource(ignoreResourceNotFound = true, value = "classpath:application.properties")
 @RestController
 @RequestMapping(UserController.BASE_URL)
 public class UserController {
 
     public static final String BASE_URL = "/api/v1/user";
     private final UserService userService;
+    @Value("${twilioAccountSid}")
+    private String twilioAccountSid;
+    @Value("${twilioApiKey")
+    private String twilioApiKey;
+    @Value("${twilioApiSecret")
+    private String twilioApiSecret;
+    @Value("${serviceSid}")
+    private String serviceSid;
 
     public UserController(UserService userService) {
         this.userService = userService;
@@ -42,6 +55,18 @@ public class UserController {
     @GetMapping
     public List<User> getUsers() {
         return userService.findUsers();
+    }
+
+    @GetMapping("/chatToken")
+    public String getChatToken(@RequestHeader("Authorization") String jwtTokenUser) {
+
+        ChatGrant grant = new ChatGrant();
+        grant.setServiceSid(serviceSid);
+
+        AccessToken token = new AccessToken.Builder(twilioAccountSid, twilioApiKey, twilioApiSecret)
+                .identity(jwtTokenUser).grant(grant).build();
+
+        return token.toJwt();
     }
 
 }
