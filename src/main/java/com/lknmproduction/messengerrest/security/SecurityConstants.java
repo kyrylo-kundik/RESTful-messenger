09f411592e1365361584defe894a93ebfb62aa60
kyrylo_kundik <goliathusua@gmail.com>
2019-03-30 20:05:04 +0200

    deleted unused constant

--- src/main/java/com/lknmproduction/messengerrest/security/SecurityConstants.java
+++ src/main/java/com/lknmproduction/messengerrest/security/SecurityConstants.java
@@ -6,5 +6,4 @@ public class SecurityConstants {
     public static final String TOKEN_PREFIX = "Bearer ";
     public static final String HEADER_STRING = "Authorization";
     public static final String SIGN_UP_URL = "/api/v1/customers/signUp";
-    public static final String LOGIN_URL = "/api/v1/customers/signIn";
 }
