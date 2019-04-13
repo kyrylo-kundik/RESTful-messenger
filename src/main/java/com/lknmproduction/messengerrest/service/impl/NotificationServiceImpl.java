cb474ed41167b02c62e722e722bc2ecd05afd00c
Kirill Kundik <goliathusua@gmail.com>
2019-04-13 19:10:38 +0300

    set up base post method for fcm

--- src/main/java/com/lknmproduction/messengerrest/service/impl/NotificationServiceImpl.java
+++ src/main/java/com/lknmproduction/messengerrest/service/impl/NotificationServiceImpl.java
@@ -1,18 +1,85 @@
 package com.lknmproduction.messengerrest.service.impl;
 
 import com.lknmproduction.messengerrest.service.NotificationService;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.configurationprocessor.json.JSONException;
+import org.springframework.boot.configurationprocessor.json.JSONObject;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpRequest;
+import org.springframework.http.client.ClientHttpRequestExecution;
+import org.springframework.http.client.ClientHttpRequestInterceptor;
+import org.springframework.http.client.ClientHttpResponse;
+import org.springframework.http.client.support.HttpRequestWrapper;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.CompletableFuture;
 
 @Service
+@PropertySource(ignoreResourceNotFound = true, value = "classpath:fcmCredentials/env.properties")
 public class NotificationServiceImpl implements NotificationService {
 
     private static final String FCM_URL = "https://fcm.googleapis.com/fcm/send";
 
+    @Value("${firebaseCredentialsKey}")
+    private String firebaseServerKey;
+
     @Override
-    public void sendNotifications(String title, String body, List<String> pushIds) {
+    @Async
+    public CompletableFuture<String> sendNotifications(String title, String body, List<String> pushIds) throws IOException, JSONException {
+
+        JSONObject reqBody = new JSONObject();
+        reqBody.put("to", pushIds);
+        reqBody.put("priority", "high");
+
+        JSONObject notification = new JSONObject();
+        notification.put("title", "JSA Notification");
+        notification.put("body", "Happy Message!");
+
+        JSONObject data = new JSONObject();
+        data.put("Key-1", "JSA Data 1");
+        data.put("Key-2", "JSA Data 2");
+
+        reqBody.put("notification", notification);
+        reqBody.put("data", data);
+
+        HttpEntity<String> request = new HttpEntity<>(reqBody.toString());
+
+        RestTemplate restTemplate = new RestTemplate();
+
+        ArrayList<ClientHttpRequestInterceptor> interceptors = new ArrayList<>();
+        interceptors.add(new HeaderRequestInterceptor("Authorization", "key=" + firebaseServerKey));
+        interceptors.add(new HeaderRequestInterceptor("Content-Type", "application/json"));
+        restTemplate.setInterceptors(interceptors);
+
+        String firebaseResponse = restTemplate.postForObject(FCM_URL, request, String.class);
+
+        return CompletableFuture.completedFuture(firebaseResponse);
 
     }
 
+}
+
+class HeaderRequestInterceptor implements ClientHttpRequestInterceptor {
+
+    private final String headerName;
+    private final String headerValue;
+
+    public HeaderRequestInterceptor(String headerName, String headerValue) {
+        this.headerName = headerName;
+        this.headerValue = headerValue;
+    }
+
+    @Override
+    public ClientHttpResponse intercept(HttpRequest request, byte[] body, ClientHttpRequestExecution execution)
+            throws IOException {
+        HttpRequest wrapper = new HttpRequestWrapper(request);
+        wrapper.getHeaders().set(headerName, headerValue);
+        return execution.execute(wrapper, body);
+    }
 }
