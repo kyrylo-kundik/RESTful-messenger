5ff2d92d1ed6153aba147b649d6386a351b1dbbd
kyrylo_kundik <goliathusua@gmail.com>
2019-04-13 19:10:38 +0300

    set up base post method for fcm

--- src/main/java/com/lknmproduction/messengerrest/service/NotificationService.java
+++ src/main/java/com/lknmproduction/messengerrest/service/NotificationService.java
@@ -1,9 +1,13 @@
 package com.lknmproduction.messengerrest.service;
 
+import org.springframework.boot.configurationprocessor.json.JSONException;
+
+import java.io.IOException;
 import java.util.List;
+import java.util.concurrent.CompletableFuture;
 
 public interface NotificationService {
 
-    void sendNotifications(String title, String body, List<String> pushIds);
+    CompletableFuture<String> sendNotifications(String title, String body, List<String> pushIds) throws IOException, JSONException;
 
 }
