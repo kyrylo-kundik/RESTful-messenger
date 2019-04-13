9a79a61500235c419e6cdc24bd0756477b207e84
kyrylo_kundik <goliathusua@gmail.com>
2019-04-13 18:21:33 +0300

    set up basic url for fcm notifications

--- src/main/java/com/lknmproduction/messengerrest/service/impl/NotificationServiceImpl.java
+++ src/main/java/com/lknmproduction/messengerrest/service/impl/NotificationServiceImpl.java
@@ -8,6 +8,8 @@ import java.util.List;
 @Service
 public class NotificationServiceImpl implements NotificationService {
 
+    private static final String FCM_URL = "https://fcm.googleapis.com/fcm/send";
+
     @Override
     public void sendNotifications(String title, String body, List<String> pushIds) {
 
