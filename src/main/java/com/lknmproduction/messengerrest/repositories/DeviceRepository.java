fbc11647ed5ee07b278fd32ee1590e7daac51409
kyrylo_kundik <goliathusua@gmail.com>
2019-04-14 14:57:43 +0300

    updated hibernate queries

--- src/main/java/com/lknmproduction/messengerrest/repositories/DeviceRepository.java
+++ src/main/java/com/lknmproduction/messengerrest/repositories/DeviceRepository.java
@@ -2,6 +2,7 @@ package com.lknmproduction.messengerrest.repositories;
 
 import com.lknmproduction.messengerrest.domain.Device;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
@@ -12,8 +13,10 @@ public interface DeviceRepository extends JpaRepository<Device, String> {
     List<String> findAllPushIds();
 
     @Query("UPDATE Device d SET d.pushId = ?2 WHERE d.id = ?1")
+    @Modifying
     void setPushIdToDevice(String deviceId, String pushId);
 
     @Query("UPDATE Device d SET d.isActive = ?2 WHERE d.id = ?1")
+    @Modifying
     void setDeviceIsActive(String deviceId, boolean isActive);
 }
